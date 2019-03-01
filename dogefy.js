e3d0e2b3870be2f447ed4fbc2308dc5f6ba7185f
Nimesh Ghelani <nimeshghelani@gmail.com>
2019-03-01 11:04:57 +0000

    Add more doges

--- dogefy.js
+++ dogefy.js
@@ -1,4 +1,4 @@
-urls=['http://doge2048.com/img/212/doge-gradient-212.gif','http://doge2048.com/img/212/doge-peepers-212.gif','http://doge2048.com/img/212/doge-shake-space-212.gif','http://doge2048.com/img/212/doge-prizza-212.gif','http://doge2048.com/img/212/doge-wink-212.gif'];
+urls=["http://doge2048.com/img/212/doge-derp-212.gif", "http://doge2048.com/img/212/doge-fat-212.gif", "http://doge2048.com/img/212/doge-gradient-212.gif", "http://doge2048.com/img/212/doge-hat-212.gif", "http://doge2048.com/img/212/doge-peepers-212.gif", "http://doge2048.com/img/212/doge-prizza-212.gif", "http://doge2048.com/img/212/doge-rainbow-212.gif", "http://doge2048.com/img/212/doge-shake-212.gif", "http://doge2048.com/img/212/doge-shake-space-212.gif", "http://doge2048.com/img/212/doge-sunglasses-212.gif", "http://doge2048.com/img/212/doge-wink-212.gif"];
 window.setInterval(function(){
     x=document.getElementsByTagName('img');
     for(i=0;i<x.length;i++){
